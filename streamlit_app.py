8aff05dee152a0846143c0a272815b9008bba845
wngud1004 <jh1004ben@gmail.com>
2024-08-19 21:57:42 +0900

    작업 디렉토리 출력

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,9 @@ def main():
         current_directory = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(current_directory, "auth.json")
 
-        cred = credentials.Certificate("auth.json")
+        print("Current working directory:", os.getcwd())
+
+        cred = credentials.Certificate("DoWhile/auth.json")
 
         # Firebase 앱 초기화
         firebase_admin.initialize_app(cred)
